516b3a2117be2b39065d08d3278e4ebcaa908ebe
Jo Lienhoop <joli@uni-bremen.de>
2020-07-26 01:29:32 +0100

    Fix url not found

--- replace.js
+++ replace.js
@@ -5,17 +5,13 @@ function replace() {
             images[i].src ===
             "https://www.studip.de/fileadmin/portal3/templates/gfx/studip_logo.svg"
         )
-            images[i].src = "images/stupid-logo.svg";
+            images[i].src = chrome.runtime.getURL("images/stupid-logo.svg");
     }
 }
 
 chrome.storage.sync.get(["enabled"], function (result) {
     console.log(`Enabled: ${result.enabled}`);
     if (result.enabled) {
-        var css = document.createElement("style");
-        css.innerHTML =
-            'img { content: url("images/stupid-logo.svg") !important; }';
-        document.body.appendChild(css);
         window.setInterval(replace, 3000);
         replace();
     }
